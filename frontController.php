b124403ff73a6e85414626c09220b6d68aff2479
mzijlstra <mzijlstra@gmail.com>
2021-08-24 22:11:14 +0200

    Remove php warnings from apache logs

--- frontController.php
+++ frontController.php
@@ -11,6 +11,7 @@ define("DB_USER", "cs472dbuser");
 define("DB_PASS", "WAP Passwd");
 $SEC_LVLS = array("none", "user", "admin");
 date_default_timezone_set("America/Chicago");
+error_reporting(E_ALL ^ E_WARNING);
 
 /* ******************************
  * Initialize Globals
