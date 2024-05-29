f933dca13ef2be261f23766f20f9e04ea39ab24e
Michael Zijlstra <mzijlstra@gmail.com>
2024-05-29 18:18:22 -0500

    Refactor / cleanup

--- res/js/tabKey.js
+++ res/js/tabKey.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {    
+window.addEventListener("load", () => {
     // make tab insert 4 spaces when in a textarea with markdown enabled
     function doTab(evt) {
         if (evt.key !== "Tab") {
@@ -17,27 +17,28 @@ window.addEventListener("load", () => {
             this.value = txtBegin + "    " + txtStop;
             this.selectionStart = selStart + 4;
             this.selectionEnd = selStart + 4;
-        } else {
-            // change indent level from selected text
-            if (!evt.shiftKey) { // add indent level
-                const txtBegin = this.value.substring(0, this.selectionStart);
-                const txtStop = this.value.substring(this.selectionEnd - 1);
-                const lines = this.value.substring(selStart, selStop - 1).split("\n");
-                const newLines = lines.map((line) => "    " + line);
-                const newText = newLines.join("\n");
-                this.value = txtBegin + newText + txtStop;
-                this.selectionStart = selStart;
-                this.selectionEnd = selStop + newLines.length * 4;
-            } else {  // remove indent level
-                const txtBegin = this.value.substring(0, this.selectionStart);
-                const txtStop = this.value.substring(this.selectionEnd);
-                const lines = this.value.substring(selStart, selStop).split("\n");
-                const newLines = lines.map((line) => line.substring(4));
-                const newText = newLines.join("\n");
-                this.value = txtBegin + newText + txtStop;
-                this.selectionStart = selStart;
-                this.selectionEnd = selStop - (newLines.length -1) * 4;
-            }
+            return;
+        }
+
+        // change indent level from selected text
+        if (!evt.shiftKey) { // add indent level
+            const txtBegin = this.value.substring(0, this.selectionStart);
+            const txtStop = this.value.substring(this.selectionEnd - 1);
+            const lines = this.value.substring(selStart, selStop - 1).split("\n");
+            const newLines = lines.map((line) => "    " + line);
+            const newText = newLines.join("\n");
+            this.value = txtBegin + newText + txtStop;
+            this.selectionStart = selStart;
+            this.selectionEnd = selStop + newLines.length * 4;
+        } else {  // remove indent level
+            const txtBegin = this.value.substring(0, this.selectionStart);
+            const txtStop = this.value.substring(this.selectionEnd);
+            const lines = this.value.substring(selStart, selStop).split("\n");
+            const newLines = lines.map((line) => line.substring(4));
+            const newText = newLines.join("\n");
+            this.value = txtBegin + newText + txtStop;
+            this.selectionStart = selStart;
+            this.selectionEnd = selStop - (newLines.length - 1) * 4;
         }
     }
     document.querySelectorAll('textarea').forEach((area) => {
