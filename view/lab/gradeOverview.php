206e3522b183d3f442e35a422a60c8bd3bce9885
Michael Zijlstra <mzijlstra@gmail.com>
2024-11-09 11:04:12 -0600

    Remove unneeded wording

--- view/lab/gradeOverview.php
+++ view/lab/gradeOverview.php
@@ -12,21 +12,21 @@
 </head>
 
 <body id="gradeLab" class="lab">
-    <?php include("header.php"); ?>
+    <?php include 'header.php'; ?>
     <main>
         <nav class="back" title="Back">
             <a href="../../lab">
                 <i class="fa-solid fa-arrow-left"></i>
             </a>
         </nav>
-        <?php include("areas.php"); ?>
+        <?php include 'areas.php'; ?>
         <div id="content">
             <h2>Lab: <?= $lab['name'] ?></h2>
             <!-- Links to grade by question -->
             <div id="deliverables">
                 <strong>Grade Deliverable:</strong>
                 <?php $count = 1; ?>
-                <?php foreach ($deliverables as $deliv) : ?>
+                <?php foreach ($deliverables as $deliv) { ?>
                     <?php
                     $gradeStatus = '';
                     if ($deliv['answers'] != 0) {
@@ -39,104 +39,105 @@
                     <a href="deliverable/<?= $deliv['id'] ?>" class="<?= $gradeStatus ?>" title="<?= $gradeStatus ?>">
                         D<?= $count++ ?>(<?= number_format($deliv['avgPoints'], 1) ?>)
                     </a>
-                <?php endforeach; ?>
+                <?php } ?>
             </div>
 
-            <?php if ($absent) : ?>
+            <?php if ($absent) { ?>
                 <h3 title="Students that have not started yet">Not Started</h3>
                 <table>
                     <tr>
                         <th>Name</th>
                     </tr>
-                    <?php foreach ($absent as $student) : ?>
+                    <?php foreach ($absent as $student) { ?>
                         <tr>
                             <td>
-                                <?php if ($lab['type'] == "individual"): ?>
+                                <?php if ($lab['type'] == 'individual') { ?>
                                 <a href="user/<?= $student['id'] ?>">
                                     <?= $student['knownAs'] ?> <?= $student['lastname'] ?>
                                 </a>
-                                <?php else: ?>
+                                <?php } else { ?>
                                     <a href="group/<?= $student ?>">
-                                        Group <?= $student ?>
+                                        <?= $student ?>
                                     </a>
-                                <?php endif; ?>
+                                <?php } ?>
                             </td>
                         </t>
-                    <?php endforeach; ?>
+                    <?php } ?>
                 </table>
-            <?php endif; ?>
+            <?php } ?>
 
             <?php function shortDate($date)
             {
-                if (!$date) {
+                if (! $date) {
                     return '';
                 }
                 $day = substr($date, 8, 2);
                 $time = substr($date, 11, 5);
+
                 return "{$day}<sup>th</sup> {$time}";
             } ?>
             <?php function submissionTable($list, $title, $titleDetail, $showDetails, $students, $members)
             { ?>
 
-                <?php if ($list) : ?>
+                <?php if ($list) { ?>
                     <!-- Table showing results of enrolled students -->
                     <h3 title="<?= $titleDetail ?>"><?= $title ?></h3>
                     <table>
                         <tr>
                             <th>Name</th>
-                            <?php if ($showDetails) : ?>
+                            <?php if ($showDetails) { ?>
                                 <th>Start</th>
                                 <th>Stop</th>
                                 <th>Delivs</th>
                                 <th>Points</th>
-                            <?php endif; ?>
+                            <?php } ?>
                         </tr>
-                        <?php foreach ($list as $submission) : ?>
+                        <?php foreach ($list as $submission) { ?>
                             <tr>
                                 <td>
                                     <?php
                                     $gradeStatus = '';
-                                    if ($submission['delivs'] != 0) {
-                                        $gradeStatus = 'graded';
-                                        if ($submission['ungraded'] != 0) {
-                                            $gradeStatus = 'ungraded';
-                                        }
-                                    }
-                                    ?>
+                            if ($submission['delivs'] != 0) {
+                                $gradeStatus = 'graded';
+                                if ($submission['ungraded'] != 0) {
+                                    $gradeStatus = 'ungraded';
+                                }
+                            }
+                            ?>
                                     <a href="submission/<?= $submission['id'] ?>" class="<?= $gradeStatus ?>" title="<?= $gradeStatus ?>">
-                                        <?php if ($submission['group']) : ?>
+                                        <?php if ($submission['group']) { ?>
                                             Group <?= $submission['group'] ?>:
                                             <span class="members">
-                                                <?php foreach ($members[$submission['group']] as $member) : ?>
+                                                <?php foreach ($members[$submission['group']] as $member) { ?>
                                                     <?= $member['knownAs'] ?> <?= $member['lastname'] ?>,
-                                                <?php endforeach; ?>
+                                                <?php } ?>
                                             </span>
-                                        <?php else : ?>
+                                        <?php } else { ?>
                                             <?php $student = $students[$submission['user_id']] ?>
                                             <?= $student['knownAs'] ?> <?= $student['lastname'] ?>
-                                        <?php endif; ?>
+                                        <?php } ?>
                                     </a>
                                 </td>
-                                <?php if ($showDetails) : ?>
+                                <?php if ($showDetails) { ?>
                                     <td class="start" title="<?= $submission['start'] ?>"><?= shortDate($submission['start']) ?> </td>
                                     <td class="stop" title="<?= $submission['stop'] ?> "><?= shortDate($submission['stop']) ?> </td>
                                     <td class="delivs"><?= $submission['delivs'] ?></td>
                                     <td class="points">
-                                        <?php if ($submission['points']) : ?>
+                                        <?php if ($submission['points']) { ?>
                                             <?= $submission['points'] == floor($submission['points']) ? $submission['points'] : number_format($submission['points'], 2) ?>
-                                        <?php else : ?>
+                                        <?php } else { ?>
                                             0
-                                        <?php endif; ?>
+                                        <?php } ?>
                                     </td>
-                                <?php endif; ?>
+                                <?php } ?>
                             </tr>
-                        <?php endforeach; ?>
+                        <?php } ?>
                     </table>
-                <?php endif; ?>
+                <?php } ?>
             <?php } ?>
-            <?php submissionTable($none, "Started", "", false, $students, $groups); ?>
-            <?php submissionTable($taken, "Results", "Submissions from enrolled students", true, $students, $members); ?>
-            <?php submissionTable($extra, "Extra", "Submissions from unenrolled students", true, $observers, $members); ?>
+            <?php submissionTable($none, 'Started', '', false, $students, $groups); ?>
+            <?php submissionTable($taken, 'Results', 'Submissions from enrolled students', true, $students, $members); ?>
+            <?php submissionTable($extra, 'Extra', 'Submissions from unenrolled students', true, $observers, $members); ?>
         </div>
     </main>
 </body>
