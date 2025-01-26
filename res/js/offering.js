50b2916df1ad922d914f84c92073b8f90f0e1555
Michael Zijlstra <mzijlstra@gmail.com>
2025-01-26 17:06:13 +0100

    Remove React, make edit Title AJAX

--- res/js/offering.js
+++ res/js/offering.js
@@ -1,8 +1,75 @@
 window.addEventListener("load", () => {
-    'use strict';
     document.getElementById("days").onclick = function(e) {
         if (e.target.classList.contains("data")) {
             e.target.querySelector("a").click();
         }
     }
+
+    // beyond here is instructor / admin code
+    const clone = document.getElementById("clone");
+    if (!clone) {
+        return;
+    }
+
+    // clone dialog
+    document.getElementById("clone").onclick = function() {
+        document.getElementById("cloneDialog").showModal();
+    }
+    document.getElementById("closeCloneDialog").onclick = function() {
+        document.getElementById("cloneDialog").close();
+    }
+
+    // delete dialog
+    document.getElementById("delete").onclick = function() {
+        document.getElementById("deleteDialog").showModal();
+    }
+    document.getElementById("closeDeleteDialog").onclick = function() {
+        document.getElementById("deleteDialog").close();
+    }
+    document.getElementById("cancel_delete").onclick = function() {
+        document.getElementById("deleteDialog").close();
+    }
+
+    // edit dialog
+    let dayId = null;
+    function showEditDialog() {
+        dayId = this.closest("div.data").id; // Like W1D1
+        const day_id = this.dataset.day_id;
+        const desc = this.previousElementSibling.innerText;
+        document.getElementById("day_id").value = day_id;
+        document.getElementById("day_desc").value = desc;
+        document.getElementById("editDialog").showModal();
+    }
+    const editIcons = document.querySelectorAll("div#days i.far.fa-edit");
+    editIcons.forEach((edit) => {
+        edit.onclick = showEditDialog;
+    });
+    document.getElementById("edit").onclick = function() {
+        editIcons.forEach((edit) => {
+            edit.classList.toggle("hide");
+        });
+    }
+    document.getElementById("closeEditDialog").onclick = function() {
+        document.getElementById("editDialog").close();
+    }
+    document.getElementById("editForm").onsubmit = function() {
+        const day_id = document.getElementById("day_id").value;
+        const day_desc = document.getElementById("day_desc").value;
+        const data = new FormData();
+        data.append("day_id", day_id);
+        data.append("desc", day_desc);
+        fetch("edit", {
+            method: "POST",
+            body: data,
+        }).then(response => {
+            if (!response.ok) {
+                alert("Edit title failed");
+                return;
+            }
+            const div = document.getElementById(dayId);
+            div.querySelector("span.text").innerText = day_desc;
+        });
+        document.getElementById("editDialog").close();
+        return false;
+    };
 });
--- view/course/overview.php
+++ view/course/overview.php
@@ -8,14 +8,8 @@
     <link rel="stylesheet" href="res/css/lib/font-awesome-all.min.css">
     <link rel="stylesheet" href="res/css/common-1.3.css">
     <link rel="stylesheet" href="res/css/offering-1.3.css">
+    <link rel="stylesheet" href="res/css/adm-1.0.css">
     <script src="res/js/offering.js"></script>
-    <?php if (hasMinAuth('instructor')) { ?>
-        <link rel="stylesheet" href="res/css/adm-1.0.css">
-        <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
-        <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
-        <script src="res/js/info.js"></script>
-        <script src="res/js/adm_offering.js"></script>
-    <?php } ?>
     <style>
         div#days {
             grid-template-columns: <?php for ($i = 0; $i < $offering['lessonsPerPart']; $i++) { ?>auto <?php } ?>;
@@ -63,12 +57,14 @@
                 <?php for ($d = 1; $d <= $offering['lessonsPerPart']; $d++) { ?>
                     <?php $date = $start + ($w - 1) * 60 * 60 * 24 * $offering['daysPerLesson'] * $offering['lessonsPerPart'] + ($d - 1) * 60 * 60 * 24 * $offering['daysPerLesson']; ?>
 
-                    <div class="data <?= $w == 1 ? 'w1' : '' ?> <?= $d == 1 ? 'd1 ' : '' ?><?= $offering['showDates'] && $date < $now ? 'done' : '' ?> <?= date('z', $date) == date('z', $now) ? 'curr' : '' ?>" id="<?= "W{$w}D{$d}" ?>" data-day="<?= "W{$w}D{$d}" ?>" data-day_id="<?= $days["W{$w}D{$d}"]['id'] ?>" data-text="<?= $days["W{$w}D{$d}"]['desc'] ?>">
+                    <div class="data <?= $w == 1 ? 'w1' : '' ?> <?= $d == 1 ? 'd1 ' : '' ?><?= $offering['showDates'] && $date < $now ? 'done' : '' ?> <?= date('z', $date) == date('z', $now) ? 'curr' : '' ?>" id="<?= "W{$w}D{$d}" ?>" data-day="<?= "W{$w}D{$d}" ?>"  data-text="<?= $days["W{$w}D{$d}"]['desc'] ?>">
                         <div class="info"></div>
                         <a href="W<?= $w ?>D<?= $d ?>/">
-                            <span class="day"><?= "W{$w}D{$d}" ?></span>
                             <span class="text"><?= $days["W{$w}D{$d}"]['desc'] ?></span>
                         </a>
+                        <i class="far fa-edit hide" 
+                            title="Edit Day Title"
+                            data-day_id="<?= $days["W{$w}D{$d}"]['id'] ?>"></i>
                         <?php if ($offering['showDates']) { ?>
                             <time><?= date('M j Y', $date); ?></time>
                         <?php } ?>
@@ -81,104 +77,104 @@
             <div class="info"></div>
         </div>
     </main>
+
     <?php if (hasMinAuth('instructor')) { ?>
-        <div id="overlay">
-            <i id="close-overlay" class="fas fa-times-circle"></i>
-            <div id="clone_modal" class="modal hide">
-                <h2>Clone Offering</h2>
-                <form method="POST" action="clone" id="clone_form">
-                    <input type="hidden" name="offering_id" value=<?= $offering['id'] ?> />
-                    <div class="line">
-                        <label>New Block:</label>
-                        <input name="block" id="block" required pattern="20\d{2}-\d{2}[^\/]*" title="Block code"/>
-                    </div>
-                    <div class="line">
-                        <label>Faculty</label>
-                        <select name="fac_user_id" id="fac_user_id">
-                            <?php foreach ($faculty as $user) { ?>
-                                <option value="<?= $user['id'] ?>"><?= $user['firstname'].' '.$user['lastname'] ?></option>
-                            <?php } ?>
-                        </select>
-                    </div>
-                    <div class="line">
-                        <label>Start Date:</label>
-                        <input type="date" name="start" required />
-                    </div>
-                    <div>
-                        <label>Days per Lesson</label>
-                        <input type="number" name="daysPerLesson" value="<?= $offering['daysPerLesson'] ?>" />
-                        <label>Lessons per Part (cols)</label>
-                        <input type="number" name="lessonsPerPart" value="<?= $offering['lessonsPerPart'] ?>" />
-                        <label>Parts (rows)</label>
-                        <input type="number" name="lessonParts" value="<?= $offering['lessonParts'] ?>" />
-                    </div>
-                    <div class="indent">
-                        <label class="checkbox">
-                            <input type="checkbox" name="hasQuiz" value="1" <?= $offering['hasQuiz'] ? 'checked' : '' ?> />
-                            Has Quiz
-                        </label>
-                        <label class="checkbox">
-                            <input type="checkbox" name="hasLab" value="1" <?= $offering['hasLab'] ? 'checked' : '' ?> />
-                            Has Lab
-                        </label>
-                        <label class="checkbox">
-                            <input type="checkbox" name="showDates" value="1" <?= $offering['showDates'] ? 'checked' : '' ?> />
-                            Show Dates
-                        </label>
-                        <label class="checkbox">
-                            <input type="checkbox" name="usesFlowcharts" value="1" <?= $offering['usesFlowcharts'] ? 'checked' : '' ?> />
-                            Flowcharts
-                        </label>
+        <dialog id="cloneDialog" class="modal">
+            <i id="closeCloneDialog" class="fas fa-times-circle close"></i>
+            <h3>Clone Offering</h3>
 
-                    </div>
-                    <div class="indent">
-                        <label class="checkbox">
-                            <input type="checkbox" name="cloneFiles" value="1" checked />
-                            Clone Public Files
-                        </label>
-                        <label class="checkbox">
-                            <input type="checkbox" name="cloneComments" value="1" />
-                            Clone Comments
-                        </label>
-                    </div>
-                    <div class="submit">
-                        <button>Submit</button>
-                    </div>
-                </form>
-            </div>
-            <div id="delete_modal" class="modal hide">
-                <h2>Delete Offering</h2>
-                <p>
-                    Please confirm that you want to delete this offering.
-                </p>
-                <form method="POST" action="delete">
-                    <input type="hidden" name="offering_id" value=<?= $offering['id'] ?> />
-                    <div class="submit">
-                        <button type="button" id="cancel_delete">Cancel</button>
-                        <button type="submit" id="ok_delete">OK</button>
-                    </div>
-                </form>
-            </div>
-            <div id="edit_modal" class="modal hide">
-                <h2>Edit Day Title</h2>
-                <form method="POST" action="edit">
-                    <input type="hidden" name="day_id" id="day_id" value="" />
-                    <div class="line">
-                        <label>Title:</label>
-                        <input name="desc" id="day_desc" placeholder="" />
-                    </div>
-                    <div class="submit">
-                        <button>Submit</button>
-                    </div>
-                </form>
-            </div>
+            <form method="POST" action="clone" id="clone_form">
+                <input type="hidden" name="offering_id" value=<?= $offering['id'] ?> />
+                <div class="line">
+                    <label>New Block:</label>
+                    <input name="block" id="block" required pattern="20\d{2}-\d{2}[^\/]*" title="Block code"
+                    autofocus />
+                </div>
+                <div class="line">
+                    <label>Faculty</label>
+                    <select name="fac_user_id" id="fac_user_id">
+                        <?php foreach ($faculty as $user) { ?>
+                            <option value="<?= $user['id'] ?>"><?= $user['firstname'].' '.$user['lastname'] ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="line">
+                    <label>Start Date:</label>
+                    <input type="date" name="start" required />
+                </div>
+                <div>
+                    <label>Days per Lesson</label>
+                    <input type="number" name="daysPerLesson" value="<?= $offering['daysPerLesson'] ?>" />
+                    <label>Lessons per Part (cols)</label>
+                    <input type="number" name="lessonsPerPart" value="<?= $offering['lessonsPerPart'] ?>" />
+                    <label>Parts (rows)</label>
+                    <input type="number" name="lessonParts" value="<?= $offering['lessonParts'] ?>" />
+                </div>
+                <div class="indent">
+                    <label class="checkbox">
+                        <input type="checkbox" name="hasQuiz" value="1" <?= $offering['hasQuiz'] ? 'checked' : '' ?> />
+                        Has Quiz
+                    </label>
+                    <label class="checkbox">
+                        <input type="checkbox" name="hasLab" value="1" <?= $offering['hasLab'] ? 'checked' : '' ?> />
+                        Has Lab
+                    </label>
+                    <label class="checkbox">
+                        <input type="checkbox" name="showDates" value="1" <?= $offering['showDates'] ? 'checked' : '' ?> />
+                        Show Dates
+                    </label>
+                    <label class="checkbox">
+                        <input type="checkbox" name="usesFlowcharts" value="1" <?= $offering['usesFlowcharts'] ? 'checked' : '' ?> />
+                        Flowcharts
+                    </label>
 
-            <div id="content">
-                <!-- React puts it's table with view data here -->
-            </div>
-        </div>
+                </div>
+                <div class="indent">
+                    <label class="checkbox">
+                        <input type="checkbox" name="cloneFiles" value="1" checked />
+                        Clone Public Files
+                    </label>
+                    <label class="checkbox">
+                        <input type="checkbox" name="cloneComments" value="1" />
+                        Clone Comments
+                    </label>
+                </div>
+                <div class="submit">
+                    <button>Submit</button>
+                </div>
+            </form>
+        </dialog>
+
+        <dialog id="deleteDialog" class="modal">
+            <i id="closeDeleteDialog" class="fas fa-times-circle close"></i>
+            <h3>Delete Offering</h3>
+            <p>
+                Please confirm that you want to delete this offering.
+            </p>
+            <form method="POST" action="delete">
+                <input type="hidden" name="offering_id" value=<?= $offering['id'] ?> />
+                <div class="submit">
+                    <button type="button" id="cancel_delete" autofocus>Cancel</button>
+                    <button type="submit" id="ok_delete">OK</button>
+                </div>
+            </form>
+        </dialog>
+
+        <dialog id="editDialog" class="modal">
+            <i id="closeEditDialog" class="fas fa-times-circle close"></i>
+            <h3>Edit Day Title</h3>
+            <form method="POST" action="edit" id="editForm">
+                <input type="hidden" name="day_id" id="day_id" value="" />
+                <div class="line">
+                    <label>Title:</label>
+                    <input name="desc" id="day_desc" autofocus />
+                </div>
+                <div class="submit">
+                    <button>Submit</button>
+                </div>
+            </form>
+        </dialog>
     <?php } ?>
-
 </body>
 
 </html>
