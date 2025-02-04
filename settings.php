289ff5bfe1ff42a8181aaafe7e875899de4eb972
Michael Zijlstra <mzijlstra@gmail.com>
2025-02-04 11:46:33 +0100

    Clean up / remove old react view system

--- settings.php
+++ settings.php
@@ -1,5 +1,13 @@
-define("DSN", "mysql:dbname=db_name;host=localhost");
-define("DB_USER", "user");
-define("DB_PASS", "pass");
-define("SALT", "hashing_salt");
-define("MAIL_PASS", 'smtp_password');      
+<?php
+define("DSN", "mysql:dbname=manalabs;host=localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("SALT", "Z Salty");
+
+define("MAIL_SERVER", "smtp.dreamhost.com");
+define("MAIL_USER", "videos@manalabs.org");
+define("MAIL_PASS", "eKzU!PRZ");
+define("MAIL_FROM", "videos@manalabs.org");
+define("MAIL_NAME", "Manalabs Video System");
+
+define("FILES", true);
