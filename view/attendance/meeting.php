1409b37ce9fc97ff422e009601c5300ecd2bc5be
Michael Zijlstra <mzijlstra@gmail.com>
2025-04-29 16:23:55 -0500

    Update unknown badge msg

--- view/attendance/meeting.php
+++ view/attendance/meeting.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Meeting: <?= $meeting["title"] ?></title>
+    <title>Meeting: <?= $meeting['title'] ?></title>
     <meta charset="utf-8" />
     <meta name=viewport content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="res/css/lib/font-awesome-all.min.css">
@@ -16,12 +16,12 @@
 </head>
 
 <body>
-    <?php include("header.php"); ?>
+    <?php include 'header.php'; ?>
     <main>
         <nav class="back" title="Back">
             <i class="fa-solid fa-arrow-left"></i>
         </nav>
-        <?php include("areas.php"); ?>
+        <?php include 'areas.php'; ?>
 
         <nav class="tools">
             <span class="iconContainer" id="barcodeReader" title="Start/Stop Camera Reader">
@@ -56,7 +56,7 @@
             </div>
             <div id="registerMsg" class="msg hidden">
                 <strong>Unknown badge:</strong> <span id="unknownBadge"></span>
-                Register it to a user by marking them present
+                Register it by using a checkbox in the table
                 <button id="cancelRegister">Cancel</button>
             </div>
         </div>
@@ -66,54 +66,54 @@
             <div class="meeting">
                 <h3>
                     Meeting Details:
-                    <form id="regen_form" method="post" action="regen/<?= $meeting["id"] ?>">
+                    <form id="regen_form" method="post" action="regen/<?= $meeting['id'] ?>">
                         <input type="hidden" name="session_id" value="<?= $meeting['session_id'] ?>" />
-                        <input type="hidden" name="meeting_id" value="<?= $meeting["id"] ?>" id="meeting_id" />
-                        <input type="hidden" name="start" value="<?= $meeting["start"] ?>" />
-                        <input type="hidden" name="stop" value="<?= $meeting["stop"] ?>" />
+                        <input type="hidden" name="meeting_id" value="<?= $meeting['id'] ?>" id="meeting_id" />
+                        <input type="hidden" name="start" value="<?= $meeting['start'] ?>" />
+                        <input type="hidden" name="stop" value="<?= $meeting['stop'] ?>" />
                         <i id="regen_meeting" class="fa-solid fa-rotate-right" title="Renerate Meeting Report"></i>
                     </form>
 
-                    <form id="delete_form" method="post" action="<?= $meeting["id"] ?>/delete">
+                    <form id="delete_form" method="post" action="<?= $meeting['id'] ?>/delete">
                         <i id="delete_meeting" class="far fa-trash-alt" title="Delete Meeting"></i>
                     </form>
                     <a href="<?= "../attendance/{$day['abbr']}/{$session['type']}#export" ?>">
-                        <i title="Export Session Attendance" class="fas fa-cloud-upload-alt <?= $session["status"] ?>"></i>
+                        <i title="Export Session Attendance" class="fas fa-cloud-upload-alt <?= $session['status'] ?>"></i>
                     </a>
                 </h3>
                 <form method="post" id="meeting_form">
-                    <input type="hidden" name="id" value="<?= $meeting["id"] ?>">
+                    <input type="hidden" name="id" value="<?= $meeting['id'] ?>">
                     <div>
                         <label>Title</label>
-                        <input type="text" name="title" value="<?= $meeting["title"] ?>" />
+                        <input type="text" name="title" value="<?= $meeting['title'] ?>" />
                     </div>
                     <div>
                         <label>Date</label>
-                        <input type="date" name="date" value="<?= $meeting["date"] ?>" />
+                        <input type="date" name="date" value="<?= $meeting['date'] ?>" />
                     </div>
                     <div>
                         <label>Start</label>
-                        <input type="text" name="start" value="<?= $meeting["start"] ?>" />
+                        <input type="text" name="start" value="<?= $meeting['start'] ?>" />
                     </div>
                     <div>
                         <label>Stop</label>
-                        <input type="text" name="stop" value="<?= $meeting["stop"] ?>" />
+                        <input type="text" name="stop" value="<?= $meeting['stop'] ?>" />
                     </div>
                 </form>
             </div>
 
             <div class="btns">
             </div>
-            <form id="absentForm" method="post" action="<?= $meeting["id"] ?>/absent">
+            <form id="absentForm" method="post" action="<?= $meeting['id'] ?>/absent">
                 <input id="absent_id" type="hidden" name="attendance_id" value="" />
             </form>
-            <form id="presentForm" method="post" action="<?= $meeting["id"] ?>/present">
+            <form id="presentForm" method="post" action="<?= $meeting['id'] ?>/present">
                 <input id="present_id" type="hidden" name="attendance_id" value="" />
             </form>
 
 
             <!-- Absent -->
-            <?php if ($absent) : ?>
+            <?php if ($absent) { ?>
                 <h3>
                     Absent
                     <i id="email_absent" class="far fa-paper-plane" title="Email Unexcused Absent"></i>
@@ -123,23 +123,23 @@
                         <th>Name</th>
                         <th>Excu</th>
                     </tr>
-                    <?php foreach ($absent as $missing) : ?>
-                        <tr data-id="<?= $missing["id"] //is attendance id 
-                                        ?>" id="<?= $missing["id"] ?>">
+                    <?php foreach ($absent as $missing) { ?>
+                        <tr data-id="<?= $missing['id'] // is attendance id
+                        ?>" id="<?= $missing['id'] ?>">
                             <td class="name">
-                                <a href="../../../user/<?= $missing["teamsName"] ?>"><?= $missing["knownAs"] ?> <?= $missing['lastname'] ?></a>
+                                <a href="../../../user/<?= $missing['teamsName'] ?>"><?= $missing['knownAs'] ?> <?= $missing['lastname'] ?></a>
                                 <span class="right present">present</span>
                             </td>
                             <td class="cbox" title="Excused">
-                                <input class="absent_excused" type="checkbox" name="excu" value="excu" <?= $missing["excused"] ? "checked" : "" ?> />
+                                <input class="absent_excused" type="checkbox" name="excu" value="excu" <?= $missing['excused'] ? 'checked' : '' ?> />
                             </td>
                         </tr>
-                    <?php endforeach; ?>
+                    <?php } ?>
                 </table>
-            <?php endif; ?>
+            <?php } ?>
 
             <!-- Present -->
-            <?php if ($present) : ?>
+            <?php if ($present) { ?>
                 <h3>
                     Present
                     <i id="email_tardy" class="far fa-paper-plane" title="Email Unexcused Tardy"></i>
@@ -156,53 +156,53 @@
                         <th title="In Physical Room">Phys</th>
                         <th title="Excused">Excu</th>
                     </tr>
-                    <?php foreach ($present as $student) : ?>
-                        <tr data-id="<?= $student["id"] //is attendance id, not student id 
-                                        ?>" id="<?= $student["id"] ?>">
+                    <?php foreach ($present as $student) { ?>
+                        <tr data-id="<?= $student['id'] // is attendance id, not student id
+                        ?>" id="<?= $student['id'] ?>">
                             <td class="student_id">
-                                <a href="../../../user/<?= $student["teamsName"] ?>"><?= $student["studentID"] ?></a>
+                                <a href="../../../user/<?= $student['teamsName'] ?>"><?= $student['studentID'] ?></a>
                             </td>
-                            <td class="name" title="<?= $student["start"] . " - " . $student["stop"] ?>">
-                                <a href="../../../user/<?= $student["teamsName"] ?>"><?= $student["knownAs"] ?> <?= $student['lastname'] ?></a>
+                            <td class="name" title="<?= $student['start'].' - '.$student['stop'] ?>">
+                                <a href="../../../user/<?= $student['teamsName'] ?>"><?= $student['knownAs'] ?> <?= $student['lastname'] ?></a>
                                 <span title="Click to mark absent" class="right absent">absent?</span>
                             </td>
                             <td>
-                                <input class="time start" value="<?= $student["start"] ?>">
+                                <input class="time start" value="<?= $student['start'] ?>">
                             </td>
                             <td>
-                                <input class="time stop" value="<?= $student["stop"] ?>">
+                                <input class="time stop" value="<?= $student['stop'] ?>">
                             </td>
                             <td class="cbox" title="Arrived Late">
-                                <input type="checkbox" name="late" value="late" <?= $student["arriveLate"] ? "checked" : "" ?> />
+                                <input type="checkbox" name="late" value="late" <?= $student['arriveLate'] ? 'checked' : '' ?> />
                             </td>
                             <td class="cbox" title="Missed Middle">
-                                <input type="checkbox" name="mid" value="mid" <?= $student["middleMissing"] ? "checked" : "" ?> />
+                                <input type="checkbox" name="mid" value="mid" <?= $student['middleMissing'] ? 'checked' : '' ?> />
                             </td>
                             <td class="cbox" title="Left Early">
-                                <input type="checkbox" name="left" value="left" <?= $student["leaveEarly"] ? "checked" : "" ?> />
+                                <input type="checkbox" name="left" value="left" <?= $student['leaveEarly'] ? 'checked' : '' ?> />
                             </td>
                             <td class="cbox" title="In Physical Room">
-                                <input id="<?= $student["badge"] ?>" type="checkbox" name="phys" value="phys" class="phys" <?= $student["inClass"] ? "checked" : "" ?> />
+                                <input id="<?= $student['badge'] ?>" type="checkbox" name="phys" value="phys" class="phys" <?= $student['inClass'] ? 'checked' : '' ?> />
                             </td>
                             <td class="cbox" title="Excused">
-                                <input type="checkbox" name="excu" value="excu" <?= $student["excused"] ? "checked" : "" ?> />
+                                <input type="checkbox" name="excu" value="excu" <?= $student['excused'] ? 'checked' : '' ?> />
                             </td>
                         </tr>
-                    <?php endforeach; ?>
+                    <?php } ?>
                 </table>
-            <?php endif; ?>
+            <?php } ?>
 
             <!-- Visitors -->
-            <?php if ($visitors) : ?>
+            <?php if ($visitors) { ?>
                 <h3>Not Enrolled</h3>
                 <table>
-                    <?php foreach ($visitors as $visitor) : ?>
+                    <?php foreach ($visitors as $visitor) { ?>
                         <tr>
-                            <td><?= $visitor["teamsName"] ?></td>
+                            <td><?= $visitor['teamsName'] ?></td>
                         </tr>
-                    <?php endforeach; ?>
+                    <?php } ?>
                 </table>
-            <?php endif; ?>
+            <?php } ?>
 
         </div>
     </main>
